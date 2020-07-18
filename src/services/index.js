58c9e49a280f909d85bc44fb44de28668cc2b369
Martins Obayomi <obayomimartins96@gmail.com>
2020-07-18 11:38:16 +0100

    Auth service

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+import Auth from './AuthService';
+import User from './UserService';
+import Comment from './CommentService';
+import Reply from './ReplyService';
+
+export const UserService = User;
+export const AuthService = Auth;
+export const CommentService = Comment;
+export const ReplyService = Reply;
