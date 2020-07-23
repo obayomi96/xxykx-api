52500791d4bbe751ee1c9b933c9c221e33fe4888
Martins Obayomi <obayomimartins96@gmail.com>
2020-07-23 03:14:33 +0100

    Integrations Auth services Dummy data Unit testCI/CD with Travis

---
+++ dummies/data/reply.js
@@ -0,0 +1,27 @@
+const ReplyData = [
+  {
+    id: 1,
+    content: 'my first reply',
+    userId: 4,
+    commentId: 5
+  },
+  {
+    id: 2,
+    content: 'my second reply',
+    userId: 1,
+    commentId: 2
+  },
+  {
+    id: 3,
+    content: 'my third reply',
+    userId: 2,
+    commentId: 1
+  },
+  {
+    id: 4,
+    content: 'my forth reply',
+    userId: 5,
+    commentId: 3
+  }];
+
+export default ReplyData;
---
+++ dummies/data/user.js
@@ -0,0 +1,27 @@
+const UserData = [
+  {
+    id: 1,
+    email: 'martinsoluwaseun47@gmail.com',
+    name: 'Martins',
+    password: 'user1pw',
+  },
+  {
+    id: 2,
+    email: 'anthony.a@gmail.com',
+    name: 'Anthony',
+    password: 'user2pw',
+  },
+  {
+    id: 3,
+    email: 'franca@gmail.com',
+    name: 'Fransisca',
+    password: 'user3pw',
+  },
+  {
+    id: 4,
+    email: 'emmatexi@gmail.com',
+    name: 'Emmanuel',
+    password: 'user4pw',
+  }];
+
+export default UserData;
